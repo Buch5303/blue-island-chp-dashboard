5cd8927c6ba51052d91326ce7414d0ff5f1292a4
Buch5303 <gbuchanan@bballc.net>
2025-05-08 05:45:03 -0400

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import BlueIslandDashboard from './BlueIslandDashboard';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BlueIslandDashboard />
+  </React.StrictMode>
+);
